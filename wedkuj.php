433d4e79ab3ffeae4a9994ae1b1559dca38fb74a
Mateusz <75077412+Birku7@users.noreply.github.com>
2021-10-01 14:12:49 +0200

    egzamin
    bez skryptu

---
+++ wedkuj.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wędkujemy</title>
+    <link rel="stylesheet" href="styl_1.css">
+</head>
+<body>
+    <header>
+        <h1>Portal dla wędkarzy</h1>
+    </header>
+    <div id="lewy">
+        <h2>Ryby drapieżne naszych wód</h2>
+        <ul>
+        </ul>
+    </div>
+    <div id="prawy">
+    
+        <img src="ryba1.jpg" alt="Sum"><br> 
+        <a href="kwerendy.txt">Pobierz kwerendy</a>
+    </div>
+    <footer>
+        <p>Stronę Wykonał mateusz birk</p>
+    </footer>
+
+
+</body>
+</html>
